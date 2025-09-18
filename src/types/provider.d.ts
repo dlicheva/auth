4706fea3c3320a0812caf1c2fe66ba0b0f39e4d4
dlicheva <diana@ocurity.com>
2025-09-18 17:21:22 +0100

    removed facebook

--- src/types/provider.d.ts
+++ src/types/provider.d.ts
@@ -3,7 +3,7 @@ import type { StrategyOptions } from './strategy';
 import type { PartialExcept } from './utils';
 import type { Nuxt } from '@nuxt/schema';
 
-export type ProviderNames<N = ''> = 'google' | 'facebook'  | N | ((nuxt: Nuxt, strategy: StrategyOptions, ...args: any[]) => void);
+export type ProviderNames<N = ''> = 'google'   | N | ((nuxt: Nuxt, strategy: StrategyOptions, ...args: any[]) => void);
 
 export interface ImportOptions {
     name: string;
